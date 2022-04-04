e0f519f6e17789e5faed711a2cde95ab7081637c
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-04 13:01:20 +0300

    3.10: deploy app to heroku

--- index.js
+++ index.js
@@ -130,5 +130,7 @@ const isNameUnique = (name) => {
   return matches.length === 0;
 };
 
-const PORT = 3001;
-app.listen(PORT);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
